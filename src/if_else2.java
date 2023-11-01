7b60c243eba8d78d6c5b036f36ccf6dabb67820e
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-01 20:52:44 +0800

    fifth

---
+++ src/if_else2.java
@@ -0,0 +1,12 @@
+public class if_else2 {
+    public static void main (String args[]){
+        int lelaki = 11;
+        int perempuan = 5;
+        if (lelaki > 10){
+            System.out.println("tema ben10");
+        }else{
+            System.out.println("tema marvel");
+        }
+        
+    }
+}
