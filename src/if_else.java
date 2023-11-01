6b3dc82649702824f830f9fd4aa17b9be2c19d5b
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-01 20:39:44 +0800

    fourth

---
+++ src/if_else.java
@@ -0,0 +1,13 @@
+public class if_else {
+    public static void main (String args[]){
+        int markah = 90;
+        if (markah > 80) {
+            System.out.println("passed");
+        }else{
+            System.out.println("grade B");
+        }
+        
+
+
+    }
+}
