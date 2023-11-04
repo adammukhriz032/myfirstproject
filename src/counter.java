c9d262c7831b0f11121ec0d9901abfee4211eeec
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-04 14:19:34 +0800

    counter

---
+++ src/counter.java
@@ -0,0 +1,9 @@
+public class counter {
+    public static void main (String args[]){
+        int number = 10;
+        while(number <= 10){
+            System.out.println(number);
+            number++;
+        }
+    }
+}
