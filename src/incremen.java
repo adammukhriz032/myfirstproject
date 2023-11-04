83435fa87ad489409731511bddd1b5654c4b9b63
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-04 14:10:31 +0800

    increment

---
+++ src/incremen.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class incremen {
+    public static void main (String args[]){
+        Scanner adam = new Scanner(System.in);
+        int masa = 5;
+        ++masa;
+        System.out.println(masa);
+    }
+}
