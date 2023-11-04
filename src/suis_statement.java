a80c96feb1310a2a72e9bbe397be8b3dec44fa16
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-04 13:59:18 +0800

    switch

---
+++ src/suis_statement.java
@@ -0,0 +1,15 @@
+public class suis_statement {
+    public static void main (String args[]){
+        int umur = 2;
+        switch(umur){
+            case 0:
+                System.out.println("adaik saya boleh meniarap");
+                break;
+            case 1:
+                System.out.println("adik saya boleh bercakap");
+                break;
+            case 2:
+                System.out.println("adik saya boleh berjalan");
+        }
+    }
+}
---
+++ src/switchstatement.java
@@ -0,0 +1,17 @@
+public class switchstatement {
+    public static void main (String args[]){
+        int umur = 2;
+        switch(umur){
+            case 0 : 
+                System.out.println("adik saya boleh meniarap");
+                break;
+            case 1 :
+                System.out.println("adik saya boleh bercakap");
+                break;
+            case 2 :
+                System.out.println("adik saya boleh berjalan");
+                break;
+
+        }
+    }
+}
