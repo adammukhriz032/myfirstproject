64bf0a127e9376a671833786bd736d0fcf24d669
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-05 12:22:04 +0800

    multiple class

---
+++ src/apple_juice1.java
@@ -0,0 +1,6 @@
+public class apple_juice1 {
+    public static void main (String args[]){
+        sandwich1 adam = new sandwich1();
+        adam.simpleMessage();
+    }
+}
---
+++ src/sandwich1.java
@@ -0,0 +1,6 @@
+public class sandwich1 {
+    
+    public void simpleMessage(){
+        System.out.println("this is another class");
+    }
+}
