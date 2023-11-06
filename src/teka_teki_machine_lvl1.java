21d79c7095dd3b1194f4c99443578dbf9d05e048
adammukhriz032 <thestartingjourney@gmail.com>
2023-11-06 22:07:42 +0800

    teka teki

---
+++ src/teka_teki_machine_lvl1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class teka_teki_machine_lvl1 {
+    public static void main (String args[]){
+        Scanner input = new Scanner(System.in);
+        int num;
+        num = 0;
+        Switch(num){
+            case 0 :
+                System.out.println("lampu apa yang dipecahkan keluar orang nya");
+                String name = input.nextLine();
+                System.out.println("Jawapan: Lampu jiran tetangga kita, Cubalah pecahkan nanti akan keluar orangnya);
+                break;
+        }
+            
+        
+
+        }
+    }
+
