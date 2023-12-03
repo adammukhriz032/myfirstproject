f571f680a4cecfa976882cabef70e5ca1c6e6533
adammukhriz032 <thestartingjourney@gmail.com>
2023-12-03 20:49:50 +0800

    for loop

---
+++ src/floop.java
@@ -0,0 +1,14 @@
+public class floop {
+    public static void main(String args[]) {
+    for(int number = 1; number <= 20; number+=2){
+        System.out.println(number);
+    }
+    }
+}
+//for/while loop = untuk kegunaan nombor
+//for loop ada 3 maklumat = 
+//1.ada nombor untuk start kiraan (0)
+//2.ada nombor untuk end(tamat) kiraan (10)
+//3.ada increment (tambah 1)
+//while loop ada 1 maklumat
+//tambah nmbr increment (++) ->  tambah 2 : +=2
